7a04456845f1e4b1f1514ec59ce5214f86691ad5
cornerloan <connorlowe21@gmail.com>
2024-08-15 18:17:02 -0700

    main menu added

--- mainmenu.gd
+++ mainmenu.gd
@@ -2,22 +2,26 @@ extends Node3D
 
 #@onready var game = $Control/main
 
-func _on_wep1_pressed():
-    Globals.playerWeapon = "Sword"
-    get_tree().change_scene("res://Control/main.tscn")
+func _on_weapon_1_pressed():
+	Globals.playerWeapon = "Sword"
+	get_tree().change_scene("res://Control/main.tscn")
 
-func _on_wep2_pressed():
-    Globals.playerWeapon = "Bow and Arrow"
-    get_tree().change_scene("res://Control/main.tscn")
 
-func _on_wep3_pressed():
-    Globals.playerWeapon = "Mace"
-    get_tree().change_scene("res://Control/main.tscn")
+func _on_weapon_2_pressed():
+	Globals.playerWeapon = "Bow and Arrow"
+	get_tree().change_scene("res://Control/main.tscn")
 
-func _on_wep4_pressed():
-    Globals.playerWeapon = "Flail"
-    get_tree().change_scene("res://Control/main.tscn")
 
-func _on_wep5_pressed():
-    Globals.playerWeapon = "Spear and Shield"
-    get_tree().change_scene("res://Control/main.tscn")
+func _on_weapon_3_pressed():
+	Globals.playerWeapon = "Mace"
+	get_tree().change_scene("res://Control/main.tscn")
+
+
+func _on_weapon_4_pressed():
+	Globals.playerWeapon = "Flail"
+	get_tree().change_scene("res://Control/main.tscn")
+
+
+func _on_weapon_5_pressed():
+	Globals.playerWeapon = "Spear and Shield"
+	get_tree().change_scene("res://Control/main.tscn")
