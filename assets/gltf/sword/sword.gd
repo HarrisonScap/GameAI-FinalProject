d147d339558453380b1ced003b1a1cb260639e12
cornerloan <connorlowe21@gmail.com>
2024-08-18 17:34:42 -0700

    fixed sword anim on menu

--- assets/gltf/sword/sword.gd
+++ assets/gltf/sword/sword.gd
@@ -16,7 +16,5 @@ func _process(delta):
 	# Calculate the new Y position for bobbing
 	var new_y = original_position.y + sin(Time.get_ticks_msec() * 0.001 * bobbing_speed) * bobbing_height
 
-	print("X: ", original_position.x," Y: ", new_y, " Z: ", original_position.z)
-	
 	# Update the model's position with the new Y value while keeping X and Z the same
 	global_position = Vector3(original_position.x, new_y, original_position.z)
