d8954b3f363e65454a4118155143ab690236cb4a
HarrisonScap <hscapell@ucsc.edu>
2024-08-21 22:14:22 -0400

    Installed gif supporting addon because im lazy, added banger title gif and fixed UI scaling with window

--- Globals.gd
+++ Globals.gd
@@ -4,21 +4,21 @@ extends Node
 
 # Weapon and sounds definitions
 var weapons = {
-    "Sword": {
-        "Swing": {"damage": 15, "Stamina": 15, "description": "Swing your sword, deal 15 damage", "sound": "res://assets/audio/sword_swing.wav"},
-        "Riposte": {"damage": 15, "Stamina": 30, "description": "Block and attack deal 15 damage", "sound": "res://assets/audio/sword_riposte.wav"},
-        "Spin Attack": {"damage": 20, "Stamina": 40, "description": "Spin around and deal 20 damage, cannot be blocked", "sound": "res://assets/audio/sword_spin_attack.wav"}
-    },
-    "Spear and Shield": {
-        "Stab": {"damage": 10, "Stamina": 15, "description": "Stab with your spear, deal 10 damage and start bleeding", "sound": "res://assets/audio/spear_stab.wav"},
-        "Shield Block": {"damage": 0, "Stamina": 15, "description": "Block with your shield, deal 0 damage", "sound": "res://assets/audio/shield_block.wav"},
-        "Shield Bash": {"damage": 10, "Stamina": 40, "description": "Slam your shield at the enemy, stunning them", "sound": "res://assets/audio/shield_bash.wav"}
-    },
-    "Mace": {
-        "Hit": {"damage": 25, "Stamina": 20, "description": "Hit with your mace, deal 25 damage", "sound": "res://assets/audio/mace_hit.wav"},
-        "Break Shield": {"damage": 15, "Stamina": 30, "description": "Negate enemy's block, dealing 15 damage", "sound": "res://assets/audio/mace_break_shield.wav"},
-        "Ground Slam": {"damage": 0, "Stamina": 15, "description": "Slam the ground stunning enemy for 1 turn", "sound": "res://assets/audio/mace_ground_slam.wav"}
-    }
+	"Sword": {
+		"Swing": {"damage": 15, "Stamina": 15, "description": "Swing your sword, deal 15 damage"},#, "sound": "res://assets/audio/sword_swing.wav"},
+		"Riposte": {"damage": 15, "Stamina": 30, "description": "Block and attack deal 15 damage"},#, "sound": "res://assets/audio/sword_riposte.wav"},
+		"Spin Attack": {"damage": 20, "Stamina": 40, "description": "Spin around and deal 20 damage, cannot be blocked"}#, "sound": "res://assets/audio/sword_spin_attack.wav"}
+	},
+	"Spear and Shield": {
+		"Stab": {"damage": 10, "Stamina": 15, "description": "Stab with your spear, deal 10 damage and start bleeding"},#, "sound": "res://assets/audio/spear_stab.wav"},
+		"Shield Block": {"damage": 0, "Stamina": 15, "description": "Block with your shield, deal 0 damage"},#, "sound": "res://assets/audio/shield_block.wav"},
+		"Shield Bash": {"damage": 10, "Stamina": 40, "description": "Slam your shield at the enemy, stunning them"},#, "sound": "res://assets/audio/shield_bash.wav"}
+	},
+	"Mace": {
+		"Hit": {"damage": 25, "Stamina": 20, "description": "Hit with your mace, deal 25 damage"},#, "sound": "res://assets/audio/mace_hit.wav"},
+		"Break Shield": {"damage": 15, "Stamina": 30, "description": "Negate enemy's block, dealing 15 damage"},#, "sound": "res://assets/audio/mace_break_shield.wav"},
+		"Ground Slam": {"damage": 0, "Stamina": 15, "description": "Slam the ground stunning enemy for 1 turn"}#, "sound": "res://assets/audio/mace_ground_slam.wav"}
+	}
 }
 
 
