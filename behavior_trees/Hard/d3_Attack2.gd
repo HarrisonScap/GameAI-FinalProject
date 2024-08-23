9d268cb3cf275a7ca4b9579c3a56326d03c2d580
HarrisonScap <hscapell@ucsc.edu>
2024-08-23 00:59:24 -0400

    Added a couple tweaks to the hard tree

--- behavior_trees/Hard/d3_Attack2.gd
+++ behavior_trees/Hard/d3_Attack2.gd
@@ -1,5 +1,8 @@
 class_name d3_Attack2 extends ActionLeaf
 
 func tick(actor: Node, _blackboard: Blackboard) -> int:
-	Globals.enemyMove = Globals.weapons[Globals.enemyWeapon].keys()[1]
-	return SUCCESS
+	if (Globals.weapons[Globals.enemyWeapon].keys()[1] in Globals.block_moves) and Globals.playerStamina <= 15:
+			return FAILURE
+	else: 
+			Globals.enemyMove = Globals.weapons[Globals.enemyWeapon].keys()[1]
+			return SUCCESS
--- behavior_trees/Hard/d3_Attack3.gd
+++ behavior_trees/Hard/d3_Attack3.gd
@@ -1,5 +1,8 @@
 class_name d3_Attack3 extends ActionLeaf
 
 func tick(actor: Node, _blackboard: Blackboard) -> int:
-	Globals.enemyMove = Globals.weapons[Globals.enemyWeapon].keys()[2]
-	return SUCCESS
+	if (Globals.weapons[Globals.enemyWeapon].keys()[2] in Globals.block_moves) and Globals.playerStamina <= 15:
+			return FAILURE
+	else: 
+			Globals.enemyMove = Globals.weapons[Globals.enemyWeapon].keys()[2]
+			return SUCCESS
